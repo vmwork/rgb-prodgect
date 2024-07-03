dc08ec0164941f04f91bd403e584d99873ef0bb3
V.Martseniuk <v.martseniuk@asbis.com>
2024-07-03 07:15:46 +0300

    desctop

---
+++ src/js/01_main.js
@@ -0,0 +1,21 @@
+// const checkWindowSize = () => {
+//   if (window.matchMedia("(min-width: 1040px)").matches) {
+//     const aboutComponent = document.querySelectorAll(".about");
+//     aboutComponent[0].classList.remove("about-desctop");
+//     aboutComponent[1].classList.remove("about-mobile");
+
+//     console.log(aboutComponent[0]);
+//     console.log("Screen width is at least 1040px");
+//   } else {
+//     aboutComponent[1].classList.add("about-mobile");
+//     aboutComponent[0].classList.remove("about-desctop");
+
+//     console.log("Screen less than 1040px");
+//   }
+// };
+// checkWindowSize();
+// window.addEventListener("resize", function () {
+//   console.log("Screen less than 1040px");
+
+//   checkWindowSize();
+// });
--- tasks/fonts.js
+++ tasks/fonts.js
@@ -1,22 +1,26 @@
-const fs = require('fs');
-const chalk = require('chalk');
+const fs = require("fs");
+const chalk = require("chalk");
 
-let srcFonts = 'src/scss/_local-fonts.scss';
-let appFonts = 'build/fonts/';
+let srcFonts = "src/scss/_local-fonts.scss";
+let appFonts = "build/fonts/";
 module.exports = function fonts(done) {
-  fs.writeFile(srcFonts, '', () => {});
+  fs.writeFile(srcFonts, "", () => {});
   fs.readdir(appFonts, (err, items) => {
     if (items) {
       let c_fontname;
       for (let i = 0; i < items.length; i++) {
-        let fontname = items[i].split('.'),
+        let fontname = items[i].split("."),
           fontExt;
         fontExt = fontname[1];
         fontname = fontname[0];
         if (c_fontname != fontname) {
-          if (fontExt == 'woff' || fontExt == 'woff2') {
-            fs.appendFile(srcFonts, `@include font-face("${fontname}", "${fontname}", 400);\r\n`, () => {});
-            console.log(chalk `
+          if (fontExt == "woff" || fontExt == "woff2") {
+            fs.appendFile(
+              srcFonts,
+              `@include font-face("${fontname}", "${fontname}", 400);\r\n`,
+              () => {}
+            );
+            console.log(chalk`
 {bold {bgGray Added new font: ${fontname}.}
 ----------------------------------------------------------------------------------
 {bgYellow.black Please, move mixin call from {cyan src/scss/_local-fonts.scss} to {cyan src/scss/global/_fonts.scss} and then change it!}}
@@ -27,6 +31,6 @@ module.exports = function fonts(done) {
         c_fontname = fontname;
       }
     }
-  })
+  });
   done();
-}
+};
