64b6c075cf8faa7241558edac2f85066d9272883
V.Martseniuk <v.martseniuk@asbis.com>
2024-07-03 12:34:24 +0300

    add tg bot

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,21 +1,56 @@
-// const checkWindowSize = () => {
-//   if (window.matchMedia("(min-width: 1040px)").matches) {
-//     const aboutComponent = document.querySelectorAll(".about");
-//     aboutComponent[0].classList.remove("about-desctop");
-//     aboutComponent[1].classList.remove("about-mobile");
+"use strict";
 
-//     console.log(aboutComponent[0]);
-//     console.log("Screen width is at least 1040px");
-//   } else {
-//     aboutComponent[1].classList.add("about-mobile");
-//     aboutComponent[0].classList.remove("about-desctop");
+const TELEGRAM_BOT_TOKEN = "7346201764:AAEHwMHDxpz1HrbBhuGS1gtQ47ACv9Q_31c";
+const TELEGRAM_CHAT_ID = "@testerRGBBotGROUPE";
+const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
 
-//     console.log("Screen less than 1040px");
-//   }
-// };
-// checkWindowSize();
-// window.addEventListener("resize", function () {
-//   console.log("Screen less than 1040px");
+const sendTelegram = async (name, phone, email) => {
+  // testerRGBBot
+  console.log(name, phone, email);
+  const text = `Заявка от ${name}!\nEmail: ${email}\nТелефон: ${phone}`;
+  console.log(text);
+  try {
+    const response = await fetch(API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chat_id: TELEGRAM_CHAT_ID,
+        text,
+      }),
+    });
 
-//   checkWindowSize();
-// });
+    if (response.ok) {
+      alert("Ваша заявка успешно отправлена");
+    } else {
+      throw new Error(response.statusText);
+    }
+    console.log(error);
+  } catch (error) {
+    console.error(error);
+    alert("Анкета не отправлена! Попробуйте позже.");
+  } finally {
+    formBtn.textContent = "Отправить";
+  }
+};
+
+const formValidate = () => {
+  const userName = document.querySelector(".user-name");
+  const userPhone = document.querySelector(".user-phone");
+  const userEmail = document.querySelector(".user-email");
+  const regexpPhone = /^(\+3|)[0-9]{10,11}$/;
+  const regexpEmail = /[^|\w](\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)/gm;
+  sendTelegram(userName.value, userPhone.value, userEmail.value);
+
+  if (userName.value !== "" && userPhone.value !== "" && userEmail.value) {
+    regexpEmail.test(userEmail.value);
+    if (!regexpPhone.test(userPhone.value)) {
+      if (!regexpEmail.test(userEmail.value)) {
+        sendTelegram(userName.value, userPhone.value, userEmail.value);
+      }
+    }
+  } else {
+    alert("Заполните форму");
+  }
+};
